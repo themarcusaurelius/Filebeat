128904446037dcd6dcc80998ef7d533b410dbf73
Mark Mayfield <mmayfield@panzura.com>
2020-04-07 10:21:34 -0500

    Uninstall Filebeat Script

---
+++ uninstallFilebeat.ps1
@@ -0,0 +1,39 @@
+﻿Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser	
+Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope LocalMachine
+Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope Process
+
+$principal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+
+if($principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Set-ExecutionPolicy Unrestricted
+
+    #Change Directory to filebeat
+    Set-Location -Path 'c:\Filebeat-master\filebeat'
+
+    #Stops filebeat from running
+    Stop-Service -Force filebeat
+
+    #Get The filebeat Status
+    Get-Service filebeat
+
+    #Change Directory to filebeat5
+    Set-Location -Path 'c:\'
+
+    "`nUninstalling Filebeat Now..."
+
+    $Target = "C:\Filebeat-master"
+
+    Get-ChildItem -Path $Target -Recurse -force |
+        Where-Object { -not ($_.pscontainer)} |
+            Remove-Item -Force -Recurse
+
+    Remove-Item -Recurse -Force $Target
+
+    "`nFilebeat Uninstall Successful."
+
+    #Close Powershell window
+    Stop-Process -Id $PID
+}
+else {
+    Start-Process -FilePath "powershell" -ArgumentList "$('-File ""')$(Get-Location)$('\')$($MyInvocation.MyCommand.Name)$('""')" -Verb runAs
+}
